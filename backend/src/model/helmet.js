af22e2de18545258bb3e547d66ac4ca56faa4f08
Sarojan-stha <myselfsarojan@gmail.com>
2026-02-17 16:11:53 +0545

    created helmet and telemetry schema

---
+++ backend/src/model/helmet.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const helmetSchema = new mongoose.Schema(
+  {
+    deviceId: {
+      type: String,
+      required: true,
+      unique: true, // every helmet has a unique device ID
+    },
+    rider: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // links helmet to a rider
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["active", "inactive", "maintenance"],
+      default: "active",
+    },
+    lastLocation: {
+      lat: Number,
+      lng: Number,
+    },
+    lastSpeed: {
+      type: Number,
+      default: 0,
+    },
+    accidentDetected: {
+      type: Boolean,
+      default: false,
+    },
+    ridingStyle: {
+      aggressive: { type: Number, default: 0 },
+      safe: { type: Number, default: 100 },
+    },
+  },
+  { timestamps: true },
+);
+
+const Helmet = mongoose.model("helmet", helmetSchema);
+module.exports = Helmet;
---
+++ backend/src/model/telemetry.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const telemetrySchema = new mongoose.Schema(
+  {
+    helmet: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Helmet",
+      required: true, // links log to a specific helmet
+    },
+    speed: {
+      type: Number,
+      required: true, // speed recorded at this timestamp
+    },
+    gps: {
+      lat: { type: Number, required: true },
+      lng: { type: Number, required: true },
+    },
+    accidentDetected: {
+      type: Boolean,
+      default: false, // true if accident detected at this timestamp
+    },
+    ridingStyle: {
+      aggressive: { type: Number, default: 0 }, // e.g., % of aggressive behavior
+      safe: { type: Number, default: 100 }, // e.g., % of safe riding
+    },
+  },
+  { timestamps: true },
+); // adds createdAt and updatedAt automatically
+
+module.exports = mongoose.model("Telemetry", telemetrySchema);
