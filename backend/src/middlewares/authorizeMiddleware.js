d2099b046948e2f3ab9928a6aaaf88c8ba310713
Sarojan-stha <myselfsarojan@gmail.com>
2026-02-17 15:52:48 +0545

    authorization middleware added

---
+++ backend/src/middlewares/authorizeMiddleware.js
@@ -0,0 +1,10 @@
+const authorizeMiddleware = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(403).json({
+      message: "Access denied",
+    });
+  }
+  next();
+};
+
+module.exports = authorizeMiddleware;
