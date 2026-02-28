6cce5a4b120022a83e86d54c0395f4f3237ce1e4
Sarojan-stha <myselfsarojan@gmail.com>
2026-03-01 01:16:47 +0545

    implemented webhooks

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const { verifyWebhook } = require("@clerk/express/webhooks");
+const User = require("../model/user");
 
 const {
   requireAuth,
@@ -62,3 +64,37 @@ router.post("/updateRole", async (req, res) => {
   res.status(200).json({ success: true });
 });
 module.exports = router;
+
+router.post(
+  "/api/webhooks",
+  express.raw({ type: "application/json" }),
+  async (req, res) => {
+    try {
+      const evt = await verifyWebhook(req);
+      const { id, first_name, last_name, username } = evt.data;
+
+      const user = await User.findOne({ clerkId: id });
+      if (!user) {
+        await User.create({
+          clerkId: id,
+          firstName: first_name,
+          lastName: last_name,
+          username: username,
+        });
+      }
+
+      // Do something with payload
+      // For this guide, log payload to console
+      const eventType = evt.type;
+      console.log(
+        `Received webhook with ID ${id} and event type of ${eventType}`,
+      );
+      console.log("Webhook payload:", evt.data);
+
+      return res.send("Webhook received");
+    } catch (err) {
+      console.error("Error verifying webhook:", err);
+      return res.status(400).send("Error verifying webhook");
+    }
+  },
+);
