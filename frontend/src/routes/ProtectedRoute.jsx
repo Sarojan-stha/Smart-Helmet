d30b513ba6b3aec81b144133dda756cd1789a85b
Sarojan-stha <myselfsarojan@gmail.com>
2026-03-02 17:49:31 +0545

    used isLoaded to prevent premature rendering

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -2,7 +2,11 @@ import { useAuth } from "@clerk/clerk-react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const { isSignedIn } = useAuth();
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return <div>Loading...</div>; // prevent premature redirect isSigned takes longer to load 
+  }
 
   if (!isSignedIn) {
     return <Navigate to="/" />;
